e0d9252ea835a22d21e9f4b52d27b27b279ef218
ErickOlguin <erickolguing@hotmail.com>
2019-02-08 12:31:48 -0600

    Comentario

---
+++ jss/main.js
@@ -0,0 +1,10 @@
+alert("Hola a todos");
+console.log("espero que esten aprendiendo")
+$("#submit-button1").on("click",function(){
+	dp();
+})
+
+function dp(){
+	var nombre =$("#name1").val();
+	alert (nombre);
+}
